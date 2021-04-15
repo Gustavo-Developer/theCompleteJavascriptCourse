8001b2c12ae2ae74328298f33573ae0b9da94a40
Gustavo Mendes <srto.mendes@gmail.com>
2021-04-15 01:34:56 -0300

    :heavy_check_mark: finished the game

--- guessMyNumber/js/script.js
+++ guessMyNumber/js/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +23,11 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#60b347'
 
         document.querySelector('.number').style.width = '30rem'
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
         // When guess is too high    
     } else if (guess > secretNumber) {
