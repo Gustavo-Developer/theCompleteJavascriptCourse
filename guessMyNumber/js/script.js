8bb285cc033a3d6ea53a615e49c14120063f817e
Gustavo Mendes <srto.mendes@gmail.com>
2021-04-15 00:52:29 -0300

    :heavy_ckeck_mark: manipuliting the css style in js

--- guessMyNumber/js/script.js
+++ guessMyNumber/js/script.js
@@ -10,12 +10,23 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
+
+    // When there is no input
     if (!guess) {
         document.querySelector('.message').textContent = '❌ No Number';
 
+
+
+        // When player wins    
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
 
+
+        document.querySelector('body').style.backgroundColor = '#60b347'
+
+        document.querySelector('.number').style.width = '30rem'
+
+        // When guess is too high    
     } else if (guess > secretNumber) {
 
         if (score > 1) {
@@ -27,6 +38,8 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = '0'
         }
 
+
+        // When guess is too low    
     } else if (guess < secretNumber) {
         document.querySelector('.message').textContent = '📉 Too low!';
         score--;
