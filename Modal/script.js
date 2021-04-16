82627a80160060141fee1ddcf9ff9bb3704d018b
Gustavo Mendes <srto.mendes@gmail.com>
2021-04-16 01:34:23 -0300

    :heavy_check_mark: Now 'ESC' can close the modal

--- Modal/script.js
+++ Modal/script.js
@@ -22,3 +22,13 @@ for (let i = 0; i < btnOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+
+
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+
+});
